e7768da314638a392bf08480fe8b566e0fcb858e
acanha <canha.andre@gmail.com>
2020-10-07 19:17:11 +0100

    forms async void example

---
+++ AsyncExperiments/AsyncForms/AsyncVoid.cs
@@ -0,0 +1,29 @@
+﻿namespace AsyncForms
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public class AsyncVoid
+    {
+        public async void CallAsyncVoid()
+        {
+            try
+            {
+                Console.WriteLine("Before throw exception");
+                await Task.Delay(1);
+                ThrowException();
+            }
+            catch (Exception ex)
+            {
+                //This line will never be reached
+                Console.WriteLine($"Exception: {ex.Message}");
+            }
+        }
+
+        private async void ThrowException()
+        {
+            await Task.Delay(1);
+            throw new Exception("My exception");
+        }
+    }
+}
--- AsyncExperiments/AsyncForms/Form1.cs
+++ AsyncExperiments/AsyncForms/Form1.cs
@@ -1,15 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace AsyncForms
+﻿namespace AsyncForms
 {
+    using System;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -17,5 +10,10 @@ namespace AsyncForms
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var asyncVoid = new AsyncVoid();
+            asyncVoid.CallAsyncVoid();
+        }
     }
 }
